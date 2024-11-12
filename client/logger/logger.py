0b9285ab983e8cc2f4b0cbc978667e359a0adc1f
hvalfangst <121831102+hvalfangst@users.noreply.github.com>
2024-11-12 14:29:29 +0100

    Implemented endpoint for receiving messages from the service bus queue

--- client/logger/logger.py
+++ client/logger/logger.py
@@ -5,4 +5,8 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
+# In order to get rid of service bus link information one has to increase logging from INFO to WARNING
+logging.getLogger("azure.servicebus").setLevel(logging.WARNING)
+logging.getLogger("azure").setLevel(logging.WARNING)
+
 logger = logging.getLogger("logger")
--- client/routers/queue.py
+++ client/routers/queue.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+import json
+
 from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from fastapi import APIRouter
 from fastapi import HTTPException
+
 from client.config import config
 from client.logger import logger
 from client.models import Person
@@ -27,14 +30,13 @@ def create_message_endpoint(person: Person) -> dict:
     try:
         # Initialize the ServiceBusClient
         client = ServiceBusClient.from_connection_string(config.CONNECTION_STRING)
-        queue_name = config.QUEUE_NAME
 
         # Serialize the message content as a JSON string
         message_content = person.json()
 
         with client:
             # Get a sender for the queue
-            sender = client.get_queue_sender(queue_name=queue_name)
+            sender = client.get_queue_sender(queue_name=config.QUEUE_NAME)
 
             with sender:
                 # Create a ServiceBusMessage
@@ -49,6 +51,70 @@ def create_message_endpoint(person: Person) -> dict:
                 # Return success message
                 return {"status": "Message sent successfully to the queue."}
 
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
+
+@router.get("/messages")
+async def receive_messages_endpoint():
+    """
+    Endpoint to receive, process and delete messages from the Azure Service Bus Queue.
+
+    :returns: A list of formatted messages or that the queue was empty
+    """
+    try:
+        # Initialize the ServiceBusClient with the connection string
+        client = ServiceBusClient.from_connection_string(conn_str=config.CONNECTION_STRING)
+
+        # Create the receiver for the queue
+        with client:
+            receiver = client.get_queue_receiver(queue_name=config.QUEUE_NAME)
+
+            with receiver:
+                logger.info(f"Starting to receive messages from queue: {config.QUEUE_NAME}")
+
+                # Receive messages
+                received_msgs = receiver.receive_messages(max_wait_time=5, max_message_count=20)
+
+                # Early return if no messages were received; this means that the queue is empty
+                if not received_msgs:
+                    logger.info("No messages received from the queue.")
+                    return {"message": "Queue is empty"}
+                else:
+                    logger.info(f"Received {len(received_msgs)} message(s) from the queue.")
+
+                # Initialize a list to store the messages to be output
+                output_messages = []
+
+                # Process, store and remove each received message
+                for msg in received_msgs:
+                    try:
+                        logger.info(f"Processing message: ID={msg.message_id}, "
+                                    f"Sequence={msg.sequence_number}, Content={msg}")
+                        if msg.body:
+                            try:
+                                # Decode the JSON payload
+                                message_json = json.loads(next(msg.body).decode('utf-8'))
+                                logger.info(f"Decoded Message JSON:\n{json.dumps(message_json, indent=2)}")
+
+                                # Append formatted message to our list of output messaged
+                                output_messages.append({
+                                    "message_id": msg.message_id,
+                                    "sequence_number": msg.sequence_number,
+                                    "content": message_json
+                                })
+
+                                # Remove the message from queue
+                                receiver.complete_message(msg)
+                                logger.info(f"Message with ID {msg.message_id} has been completed.")
+
+                                return {"messages": output_messages}
+
+                            except json.JSONDecodeError as e:
+                                logger.error(f"Error decoding JSON: {e}")
+                    except Exception as msg_error:
+                        logger.error(f"Error while processing message {msg.message_id}: {msg_error}")
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
